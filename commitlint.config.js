f32b7cffdf5fb61fba985be3922735f01efcea6f
sportgirl55 <sportgirl55@yandex.ru>
2026-03-08 18:47:20 +0700

    chore: configure eslint, prettier, husky, lint-staged, commitlint
    Настроены eslint, prettier, husky, lint-staged и commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // новая функциональность
+        "fix", // исправление бага
+        "docs", // документация
+        "style", // форматирование (не CSS!)
+        "refactor", // рефакторинг без изменения поведения
+        "perf", // улучшение производительности
+        "test", // тесты
+        "chore", // обслуживание (deps, configs)
+        "ci", // CI/CD
+        "revert", // откат коммита
+      ],
+    ],
+    // тема не длиннее 72 символов
+    "subject-max-length": [2, "always", 72],
+  },
+};
