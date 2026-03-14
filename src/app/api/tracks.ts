ce3c7f4a47eca70d520019bd385512bbf45ffc33
Роман Бандурин <roman.obj@mail.ru>
2026-03-14 09:43:32 +0300

    feat(msw): add track endpoint and tanstack-query tracks list

---
+++ src/app/api/tracks.ts
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+export interface Track {
+  _id: string
+  name: string
+  author: string
+  releaseDate: string
+  genre: string
+  durationInSeconds: number
+  album: string
+  previewUrl: string
+  trackUrl: string
+}
+
+function buildTracksUrl() {
+  const baseUrl = import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, "") ?? ""
+  return `${baseUrl}/track`
+}
+
+export async function getTracks(): Promise<Track[]> {
+  const { data } = await axios.get<Track[]>(buildTracksUrl())
+  return data
+}
