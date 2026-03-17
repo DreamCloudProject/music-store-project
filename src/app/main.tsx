98ec73a44dac947d75cc75831e3b2bf818892fec
Роман Бандурин <roman.obj@mail.ru>
2026-03-17 16:44:12 +0300

    MUS-30 added msw and tanstack query (#31)
    * feat(msw): add track endpoint and tanstack-query tracks list
    
    * fix(build): remove BOM

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,19 @@
+﻿import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
+
 import App from "./App.tsx";
+import "./styles/index.css";
+
+const { worker } = await import("./mocks/browser");
+await worker.start({ onUnhandledRequest: "bypass" });
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
 );
