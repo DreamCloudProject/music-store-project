dfd5e62107f4b62cc7e7748707fa9cbc20c72fe9
Роман Бандурин <roman.obj@mail.ru>
2026-04-24 19:54:46 +0300

    chore(refactor): lint-only cleanup and formatting

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,8 +1,8 @@
 ﻿import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { setupWorker } from "msw/browser";
 import axios from "axios";
+import { setupWorker } from "msw/browser";
 
 import { handlers } from "./mocks/handlers";
 import App from "./App.tsx";
